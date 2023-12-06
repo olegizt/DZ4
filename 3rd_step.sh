b03ecd7c3f5c9f7c28b24af71026552e18a7f493
olegizt <olegdubna@mail.ru>
2023-12-06 23:45:25 +0300

    Update 3rd_step.sh

--- 3rd_step.sh
+++ 3rd_step.sh
@@ -19,13 +19,13 @@ sudo umount /mnt
 sudo mount /dev/VolGroup00/LogVol_Home /home/
 
 
-sudo echo "`sudo blkid | grep Home | awk '{print $2}'` /home xfs defaults 0 0" >> /etc/fstab #вносим корректировки в файл fstab для монтирования вновь созданного каталога home в точке монтирования home
+sudo su -c "echo '`sudo blkid | grep Home | awk '{print $2}'` /home xfs defaults 0 0' >> /etc/fstab" #вносим корректировки в файл fstab для монтирования вновь созданного каталога home в точке монтирования home
 sudo touch /home/file{1..20} #создаём в домашнем каталоге 20ть файлов
 sudo lvcreate -L 100MB -s -n home_snap /dev/VolGroup00/LogVol_Home #делаем снэпшот текущего состояния домашнего каталога
 sudo rm -f /home/file{11..20} #удаляем часть файлов
 sudo umount /home #размонтируем каталог home
 sudo lvconvert --merge /dev/VolGroup00/home_snap #восстанавливаем каталог home из снэпшота
-sudo mount /home #монтируем каталог home
+sudo mount /dev/VolGroup00/LogVol_Home /home #монтируем каталог home
 sudo ls /home #проверяем что ранее удалённые файлы восстановились из снэпшота
 
 echo -e "\nЗадание закончено. Для выключения виртуальной машины используйте команду - shutdown now/n"
